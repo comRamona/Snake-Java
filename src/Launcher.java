6270c7eb26ee0ffede28abe4f327ee61aea501f9
Rami <com.ramona@yahoo.com>
2015-06-29 21:01:07 +0300

    Application Source code

---
+++ src/Launcher.java
@@ -0,0 +1,32 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Launcher extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4796392158436621989L;
+
+	public Launcher() {
+
+		setContentPane(new Snake());
+		setResizable(false);
+		setTitle("Address Book");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame frame = new Launcher();
+				frame.setSize(400, 500);
+				frame.setVisible(true);
+
+			}
+		});
+
+	}
+}
